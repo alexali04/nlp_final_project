05372f235b0a42a4a6fdb3854385452713d0703e
TheGreatKel <58996717+TheGreatKel@users.noreply.github.com>
2023-12-13 17:30:46 -0500

    Updated lower bound model to use training dataset.

--- models/lower_bound.py
+++ models/lower_bound.py
@@ -1,15 +1,15 @@
 import json
 import pandas as pd
-from system_eval import evaluate, print_results
+from sklearn.metrics import classification_report
 
 
 def main():
-    labeled_articles = pd.read_csv('./combined_data.csv')  # labeled articles must be in 
-    actual_bias = labeled_articles['bias_text']
-    predicted_bias = pd.DataFrame(["right"] * len(actual_bias)) # lower-bound model predicts right for each prediction. 
+    labeled_articles = pd.read_csv('./train.csv')  # labeled articles must be in 
+    actual_bias = labeled_articles['bias']
+    predicted_bias = pd.DataFrame([2] * len(actual_bias)) # lower-bound model predicts right for each prediction. 
 
-    confusion_matrices, precision_arr, recall_arr, accuracy = evaluate(actual_bias, predicted_bias)
-    print_results(confusion_matrices, precision_arr, recall_arr, accuracy)
+    report = classification_report(actual_bias, predicted_bias, target_names=['left', 'center', 'right'])
+    print(report)
 
 if __name__ == "__main__":
     main()
@@ -17,32 +17,14 @@ if __name__ == "__main__":
 
 '''
 OUTPUT:
-Left Precision: 1.0
-Left Recall: 1.0
+precision    recall  f1-score   support
 
+        left       0.00      0.00      0.00     11747
+      center       0.00      0.00      0.00      9823
+       right       0.37      1.00      0.54     12430
 
-Center Precision: 1.0
-Center Recall: 1.0
+    accuracy                           0.37     34000
+   macro avg       0.12      0.33      0.18     34000
+weighted avg       0.13      0.37      0.20     34000
 
-
-Right Precision: 0.0
-Right Recall: 0.0
-
-
-Left Confusion Matrix:
-                   Predicted: Left  Predicted: Not Left
-Actual: Left                24549                    0
-Actual: Not Left            13005                    0
-
-Center Confusion Matrix:
-                     Predicted: Center  Predicted: Not Center
-Actual: Center                  26739                      0
-Actual: Not Center              10815                      0
-
-Right Confusion Matrix:
-                    Predicted: Right  Predicted: Not Right
-Actual: Right                     0                 23820
-Actual: Not Right                 0                 13734
-
-Overall Accuracy: 0.3657133727432497
 '''
